3d4a24811fdee0697e204eb8f6f704c7a5bf32b6
mitja <mitja.kovachich@gmail.com>
2013-02-08 12:41:34 +0100

    Inject skripte v odprti tab

--- Extension/bootstrap.js
+++ Extension/bootstrap.js
@@ -1,33 +1,58 @@
 var BrowserHarvester = {
 	Config: {
-		URL_GET : 'http://localhost:8080/SwitchProxy/renderer/get-next.jsp',
+		URL_GET : 'http://localhost:8080/SwitchProxy/renderer/get-twitter.jsp',
 		URL_STORE : 'http://localhost:8080/SwitchProxy/renderer/store.jsp'
 	},
 
 	Diagnostics : {
 		log : function(message, obj) {
-			alert(message, obj);
+//			alert(message + ": " + obj);
 		}
 	},
 
-	Service : {},
+	Service : {
+		commit : function() {}
+	},
+
+	TaskScriptQueue : {},
+	CurrentTaskScript : null,
 
 	start : function() {
 
-	}
+	},
 };
 
+// Register DOM ready listener
+// Here is our point to inject scripts to build desired pages for archive
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	// Execute some script when the page is fully (DOM) ready
+    if (changeInfo.status == 'complete') {
+        chrome.tabs.executeScript(null, {
+        	code:BrowserHarvester.CurrentTaskScript
+        });
+    }
+});
+
+
 // Poll implementation
+// Spawns request to fetch rendering task form  server
 $.ajax({
 	url : BrowserHarvester.Config.URL_GET,
 	contentType: 'application/json; charset=utf-8',
 	success : function(task) {
-		BrowserHarvester.Diagnostics.log(task);
+		BrowserHarvester.Diagnostics.log("Fetched task", task);
 
 		if(task.valid) {
-			chrome.tabs.create({
+			// task is valid
+			// store script which would be executed when page is fully rendered
+			BrowserHarvester.CurrentTaskScript = task.script.content;
+
+			// open tab with url
+			var tabId = chrome.tabs.create({
 				url : task.url
 			});
+
+			BrowserHarvester.Diagnostics.log("TAB-ID", tabId);
 		}
 	},
 	error: function(xhr, text, err) {
