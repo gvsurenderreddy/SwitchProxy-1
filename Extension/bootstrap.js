133beb4702df00ce949ba35bc7a1cd92ea518bac
mitja <mitja.kovachich@gmail.com>
2013-02-11 13:53:46 +0100

    Popravki in refaktorizacije

--- Extension/bootstrap.js
+++ Extension/bootstrap.js
@@ -1,82 +1,134 @@
 var BrowserHarvester = {
 	Config: {
-		URL_GET : 'http://localhost:8080/SwitchProxy/renderer/get-twitter.jsp',
-		URL_STORE : 'http://localhost:8080/SwitchProxy/renderer/store.jsp'
+		URL_GET : 'http://localhost:8080/SwitchProxy/renderer-interface',
+		URL_STORE : 'http://localhost:8080/SwitchProxy/renderer-interface'
 	},
 
-	Diagnostics : {
-		log : function(message, obj) {
-//			alert(message + ": " + obj);
-		}
+	// -----------------------------------------------------------------------------------------
+	Log:  {
+		debug: function(message, obj) {
+			BrowserHarvester.log(message, obj);
+		},
+		info: function(message, obj) {
+			BrowserHarvester.log(message, obj);
+		},
+		warn: function(message, obj) {
+			BrowserHarvester.log(message, obj);
+		},
+		error: function(message, obj) {
+			BrowserHarvester.log(message, obj);
+		}	
+	},
+	log : function(message, obj) {
+		console.log(message);
+		if(obj != null) console.log(obj);
 	},
 
+	// -----------------------------------------------------------------------------------------
 	Service : {
-		commit : function() {}
+		/**
+		 * Commits operation to server
+		 */
+		commit : function(data) {
+			$.ajax({
+				url: BrowserHarvester.Config.URL_STORE,
+				type: 'post',
+				data : {
+					"content" : data,
+					"id" : BrowserHarvester.CurrentTaskId
+				},
+				success : function(response) {
+					// Reset
+					BrowserHarvester.CurrentTaskScript = null;
+					BrowserHarvester.CurrentTaskId = null;			
+
+					// resume
+					// close tabl, cleanup
+				},
+				error: function(xhr, text, err) {
+					alert([err]);
+				}
+			});
+
+		},
+
+		// Poll implementation
+		// Spawns request to fetch rendering task form  server		
+		poll : function() {
+			$.ajax({
+				url : BrowserHarvester.Config.URL_GET,
+				contentType: 'application/json; charset=utf-8',
+				success : function(task) {
+					BrowserHarvester.log("Poll task", task);
+
+					if(task.valid) {
+						BrowserHarvester.log("New task(ID: " + task.id +
+							"; scrit-lenght: " + task.rule.clientScript +
+							") acquired for url: " + task.url);
+						// task is valid
+						// store script which would be executed when page is fully rendered
+						BrowserHarvester.CurrentTaskScript = task.rule.clientScript;
+						BrowserHarvester.CurrentTaskId = task.id;
+
+						// open tab with url
+						var tabId = chrome.tabs.create({
+							url : task.url
+						});
+
+						BrowserHarvester.Log.debug("TAB-ID", tabId);
+					}
+					else {
+						// setTimeout
+						// loop
+						setTimeout(BrowserHarvester.Service.poll, 1000);
+					}
+				},
+				error: function(xhr, text, err) {
+					alert([xhr, err]);
+				}
+			});			
+		}
 	},
 
 	TaskScriptQueue : {},
 	CurrentTaskScript : null,
+	CurrentTaskId : null,
 
 	start : function() {
+		BrowserHarvester.log("Browser renderer started");
 
+		// Listens to messages from content-script: event.urlloaded.js
+		// Listens to messages from content script, which extracts and passes serialized DOM		
+		chrome.extension.onMessage.addListener(function(data, sender, sendResponse) {
+			BrowserHarvester.Service.commit(data);
+		});
+
+		// Register DOM ready listener
+		// Here is our point to inject scripts to build desired pages for archive
+		chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+			// Execute some script when the page is fully (DOM) ready
+		    if (changeInfo.status == 'complete') {
+		    	BrowserHarvester.Log.info("DOM ready, injecting script.");
+		    	BrowserHarvester.Log.debug(BrowserHarvester.CurrentTaskScript);
+
+		        chrome.tabs.executeScript(null, {
+		        	code:BrowserHarvester.CurrentTaskScript
+		        });
+		    }
+		});		
+
+		BrowserHarvester.Service.poll();
 	},
 };
 
-// Register DOM ready listener
-// Here is our point to inject scripts to build desired pages for archive
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	// Execute some script when the page is fully (DOM) ready
-    if (changeInfo.status == 'complete') {
-        chrome.tabs.executeScript(null, {
-        	code:BrowserHarvester.CurrentTaskScript
-        });
-    }
-});
-
-chrome.extension.onMessage.addListener(function(data, sender, sendResponse) {
-	$.ajax({
-		url: BrowserHarvester.Config.URL_STORE,
-		type: 'post',
-		data : {
-			"htmlContent" : data
-		},
-		success : function(response) {
-			// resume
-		},
-		error: function(xhr, text, err) {
-			alert([err]);
-		}
-	});
-});
+BrowserHarvester.start();
 
 
-// Poll implementation
-// Spawns request to fetch rendering task form  server
-$.ajax({
-	url : BrowserHarvester.Config.URL_GET,
-	contentType: 'application/json; charset=utf-8',
-	success : function(task) {
-		BrowserHarvester.Diagnostics.log("Fetched task", task);
 
-		if(task.valid) {
-			// task is valid
-			// store script which would be executed when page is fully rendered
-			BrowserHarvester.CurrentTaskScript = task.script.content;
 
-			// open tab with url
-			var tabId = chrome.tabs.create({
-				url : task.url
-			});
 
-			BrowserHarvester.Diagnostics.log("TAB-ID", tabId);
-		}
-		else {
-			// setTimeout
-			// loop
-		}
-	},
-	error: function(xhr, text, err) {
-		alert([err]);
-	}
-});
+
+
+
+
 
--- Extension/event.urlloaded.js
+++ Extension/event.urlloaded.js
@@ -1,3 +1,11 @@
+/**
+ * Script which is injected into DOM of harvesteing page to provide 
+ * utility function.
+ */
+
+ /**
+  * Pass serialized DOM to extension, which then send content to Proxy server
+  */
 function commit() {
 	var content = document.getElementsByTagName('html')[0].innerHTML;
 	chrome.extension.sendMessage(content);
