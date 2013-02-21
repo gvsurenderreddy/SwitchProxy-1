40528dea52f1a2277ecd7ec378d37736a8032f9e
mitja <mitja.kovachich@gmail.com>
2013-02-21 21:46:08 +0100

    * delujoča integracija  jHTTPp2 proxija

---
+++ src/java/si/unilj/nuk/jhttpp2/jHTTPp2ProxyUtil.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package si.unilj.nuk.jhttpp2;
+
+/**
+ *
+ * @author mitja
+ */
+public class jHTTPp2ProxyUtil {
+	
+}
