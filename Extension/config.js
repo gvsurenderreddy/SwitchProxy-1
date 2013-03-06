f4c6f79e91a8309c17059b14ffbe681b90448bdb
mitja <mitja.kovachich@gmail.com>
2013-03-06 21:39:57 +0100

    Dodani datoteki

---
+++ Extension/config.js
@@ -0,0 +1,9 @@
+var BrowserHarvester = {
+	Config: {
+		URL_GET : 'http://localhost:9080/SwitchProxy/renderer-interface',
+		URL_STORE : 'http://localhost:9080/SwitchProxy/renderer-interface',
+
+		PROCESSING_TIMEOUT : 5*60*1000,
+		PROCESSING_TIMEOUT_RETRY : 3
+	}
+};
