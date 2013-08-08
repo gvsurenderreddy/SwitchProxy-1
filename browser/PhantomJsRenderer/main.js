3cc846df933f82cb57480e556992192c12af2eba
mitja.kovacic@nuk.uni-lj.si <>
2013-08-08 14:24:20 +0200

    Implemented: Open task's page and execute script.

--- browser/PhantomJsRenderer/main.js
+++ browser/PhantomJsRenderer/main.js
@@ -1,9 +1,16 @@
 var Page = require('webpage');
+var system = require('system');
 
 var PhantomRenderer = {
 	Config : {
 		URL_GET : 'http://localhost:8080/SwitchProxy/renderer-interface',
 		POLL_INTERVAL : 1000
+	},
+
+	Log : {
+		i : function(msg) {
+			console.log(msg);
+		}
 	},
 
 	Net : {
@@ -14,18 +21,45 @@ var PhantomRenderer = {
 				var task = JSON.parse(p.plainText);
 
 				if(task.valid) {
-
+					PhantomRenderer.Log.i("New task: " + task.id + "; URL: " + task.url);
+					PhantomRenderer.Renderer.render(task);
 				}
 				else {
 					setTimeout(PhantomRenderer.Net.next, PhantomRenderer.Config.POLL_INTERVAL);
 				}
 			});
 		}
-	}
+	},
 
+	Renderer : {
+		callbackListener : function(data) {
+			switch(data.type) {
+				case 'log':
+					PhantomRenderer.Log.i("From page context: " + data.content);
+					break;
+				case 'commit':
+					console.log(data.content);
+					PhantomRenderer.Net.next();
+					break;				
+			}
+		},
+		errorListener : function(msg, trace) {
+			// retry
+			PhantomRenderer.Net.next();
+		},
+
+		render : function(task) {
+			var page = Page.create();
+
+			page.onCallback = PhantomRenderer.Renderer.callbackListener;
+			page.open(task.url, function(status) {
+				if(status === 'success') {
+					page.evaluateJavaScript(task.rule.clientScript);
+				}
+			});
+		}
+	}
 };
 
 
-console.log(PhantomRenderer.Config.URL_GET);
 PhantomRenderer.Net.next();
-
