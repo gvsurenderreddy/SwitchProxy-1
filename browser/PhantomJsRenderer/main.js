50dd608fdc7f9ad46056953491aa68b61c603ac2
mitja.kovacic@nuk.uni-lj.si <>
2013-08-08 14:51:50 +0200

    Passing data back to proxy.

--- browser/PhantomJsRenderer/main.js
+++ browser/PhantomJsRenderer/main.js
@@ -4,6 +4,7 @@ var system = require('system');
 var PhantomRenderer = {
 	Config : {
 		URL_GET : 'http://localhost:8080/SwitchProxy/renderer-interface',
+		URL_STORE : 'http://localhost:8080/SwitchProxy/renderer-interface',
 		POLL_INTERVAL : 1000
 	},
 
@@ -11,6 +12,10 @@ var PhantomRenderer = {
 		i : function(msg) {
 			console.log(msg);
 		}
+	},
+
+	Task : {
+		current : null
 	},
 
 	Net : {
@@ -22,11 +27,33 @@ var PhantomRenderer = {
 
 				if(task.valid) {
 					PhantomRenderer.Log.i("New task: " + task.id + "; URL: " + task.url);
+
+					PhantomRenderer.Task.current = task;
+
 					PhantomRenderer.Renderer.render(task);
 				}
 				else {
 					setTimeout(PhantomRenderer.Net.next, PhantomRenderer.Config.POLL_INTERVAL);
 				}
+			});
+		},
+		store : function(data) {
+			task = PhantomRenderer.Task.current;
+			PhantomRenderer.Log.i("Storing: " + task.id + "; URL: " + task.url);
+
+			var req = Page.create();
+
+			req.open(PhantomRenderer.Config.URL_GET, 'POST', JSON.stringify({
+				"content"	: data.content,
+				"id"		: PhantomRenderer.Task.current.id,
+				"headers"	: data.headers,
+				"metadata"	: data.metadata
+			}), function(status) {
+				PhantomRenderer.Log.i("Content stored: " + status);
+
+				PhantomRenderer.Task.current = null;
+				PhantomRenderer.Log.i("Listening for new task");
+				PhantomRenderer.Net.next();
 			});
 		}
 	},
@@ -38,12 +65,18 @@ var PhantomRenderer = {
 					PhantomRenderer.Log.i("From page context: " + data.content);
 					break;
 				case 'commit':
-					console.log(data.content);
-					PhantomRenderer.Net.next();
+					PhantomRenderer.Log.i("Received commit call.");
+					PhantomRenderer.Log.i("Content-Length: " + data.content.length);
+
+					data.metadata = {};
+					data.headers = [];
+
+					PhantomRenderer.Net.store(data);
 					break;				
 			}
 		},
 		errorListener : function(msg, trace) {
+			PhantomRenderer.Log.i("Error occured: " + msg);
 			// retry
 			PhantomRenderer.Net.next();
 		},
@@ -62,4 +95,5 @@ var PhantomRenderer = {
 };
 
 
+PhantomRenderer.Log.i("Listening for tasks...");
 PhantomRenderer.Net.next();
